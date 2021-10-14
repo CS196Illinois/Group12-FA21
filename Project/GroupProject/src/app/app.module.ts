86bec1af766694414a82d8d7d6639f418f490931
Shreshta <kethireddy.shreshta@gmail.com>
2021-10-13 22:05:21 -0500

    made component pushing to see if it works

--- Project/GroupProject/src/app/app.module.ts
+++ Project/GroupProject/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemMenuComponent } from './item-menu/item-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemMenuComponent
   ],
   imports: [
     BrowserModule,
