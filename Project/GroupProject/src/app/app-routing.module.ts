06c934b9dcfd8182497f3fd93325920958e03b05
shreyapullabh <shreyapullabh@gmail.com>
2021-11-03 21:59:07 -0500

    added routing

--- Project/GroupProject/src/app/app-routing.module.ts
+++ Project/GroupProject/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ItemEditComponent } from './item-edit/item-edit.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainMenuComponent },
+  { path: 'edit-profile', component: EditProfileComponent },
+  { path: 'item-edit', component: ItemEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
