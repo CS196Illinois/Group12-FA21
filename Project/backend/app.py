7804399ebaf5e8c009b3a1172364277204acf92e
Shreshta <kethireddy.shreshta@gmail.com>
2021-12-07 16:16:53 -0600

    backend import statements

--- Project/backend/app.py
+++ Project/backend/app.py
@@ -1,7 +1,8 @@
+from flask import Flask
 import pymongo
 from pymongo import MongoClient
 
-cluster = MongoClient("mongodb+srv://srkethireddy:4qhe9j<password>@cluster0.hlsqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+cluster = MongoClient("mongodb+srv://srkethireddy:4qhe9j@cluster0.hlsqe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
 db = cluster["Items"]
 collection = db["Item"]
@@ -9,7 +10,7 @@ collection = db["Item"]
 post1 = {"_id": 0, "name": "Duck", "price": 500, "Description": "A very good duck"}
 post2 = {"_id": 0, "name": "Even Better Duck", "price": 1000, "Description": "A very good duck"  }
 
-collection.insert_many([post1, post2])
+collection.insert_many([postpip1, post2])
 
 # results = collection.find({"name" : "Duck"})
 #for result in results: 
