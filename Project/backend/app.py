d5de6036504867ee7c64cbd162932a0265ffdd6e
Shreshta <kethireddy.shreshta@gmail.com>
2021-12-08 16:34:35 -0600

    Updating the template given to students

--- Project/backend/app.py
+++ Project/backend/app.py
@@ -3,38 +3,40 @@ app = Flask(__name__)
 
 items_database = []
 
+@app.route('/test', methods=["GET"])
+def helloWorldTest():
+    return jsonify({"message": "Hello World"}), 200
 
 
-
-@app.route('/', method=["GET"])
+@app.route('/', methods=["GET"])
 def getAllItems():
     return jsonify(items_database), 200
 
 
-@app.route('/', method=["POST"])
+@app.route('/', methods=["POST"])
 def craeteItem():
     #GOOGLE HOW TO GET INFORMATION FROM BODY OF A POST might be requests.request(body)
     localDic = {} #fill out the local dict with information retrieved
     items_database.append(items_database)
-    return jsonify(last item), 200
+    return jsonify({"message": "test"}), 200
 
 
-@app.route('/<itemID>', method=["GET"])
-def getAllItems():
+@app.route('/<itemID>', methods=["GET"])
+def getItems(itemID):
     # Find the item 
     #return the item
     return jsonify(items_database), 200
 
 
-@app.route('/<itemID>', method=["PUT"])
-def updateItem():
+@app.route('/<itemID>', methods=["PUT"])
+def updateItem(itemID):
     # Find item and update it
     # return updated item
     return jsonify(items_database), 200
 
 
-@app.route('/<itemID>', method=["DELETE"])
-def updateItem():
+@app.route('/<itemID>', methods=["DELETE"])
+def deleteItem(itemID):
     # Find item and delete it
     # return deleted item
     return jsonify(items_database), 200
