30f95bff8c6e4122fa6eaeedb23e2d8bbdd6934c
sarahho2 <sarahho2@illinois.edu>
2021-12-08 20:33:59 -0600

    loads of things

--- Project/GroupProject/src/app/item-menu/item-menu.component.ts
+++ Project/GroupProject/src/app/item-menu/item-menu.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,11 +17,22 @@ export class ItemMenuComponent implements OnInit {
   email!: String;
   phoneNumber!: String;
   location!: String;
+  
+  item!: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.ItemID = this.route.snapshot.params['itemID'];
+
+    await this.http
+      .get<any>("http://127.0.0.1:5000/get/" + this.ItemID)
+      .toPromise().then(response => {
+        console.log("http://127.0.0.1:5000/" + this.ItemID)
+        console.log(response)
+        this.item = response;
+      }
+    )
   }
 
 }
