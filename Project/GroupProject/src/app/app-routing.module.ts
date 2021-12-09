e99712de8cc36d9a12ec5b3150d93aad877a4ebe
sarahho2 <sarahho2@illinois.edu>
2021-12-08 19:53:35 -0600

    More changes

--- Project/GroupProject/src/app/app-routing.module.ts
+++ Project/GroupProject/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ItemCreateComponent } from './item-create/item-create.component';
 import { ItemEditComponent } from './item-edit/item-edit.component';
 import { ItemMenuComponent } from './item-menu/item-menu.component';
 import { ItemPreviewComponent } from './item-preview/item-preview.component';
@@ -10,7 +11,8 @@ import { ProfileComponent } from './profile/profile.component';
 const routes: Routes = [
   { path: '', component: MainMenuComponent },
   { path: 'edit-profile', component: EditProfileComponent },
-  { path: 'item-edit', component: ItemEditComponent },
+  { path: 'item-edit/:itemID', component: ItemEditComponent },
+  { path: 'item-create', component: ItemCreateComponent },
   { path: 'item/:itemID', component: ItemMenuComponent }, 
   { path: 'main-menu', component: MainMenuComponent }, 
   { path: 'profile', component: ProfileComponent }, 
